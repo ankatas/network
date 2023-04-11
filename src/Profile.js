e94d370c68475d86a0fb055128c7732c7d8aa3f0
Anna <anna.katasonova@oneclicklca.com>
2023-04-11 17:07:26 +0300

    Added more lifecycle hooks

--- src/Profile.js
+++ src/Profile.js
@@ -15,8 +15,27 @@ export class Profile extends React.Component {
     });
   }
 
+  componentDidMount() {
+    this.loadUserData();
+  }
+
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+
   render() {
-    const isLoading = true;
+    const isLoading = this.state.userData === null;
+    let name = isLoading ? 'Loading...' : this.state.userData.name;
+    let bio = isLoading ? 'Unknown creature' : this.state.userData.bio;
+    let friends = isLoading ? [] : this.state.userData.friends;
+    let avatar = isLoading ? '' : this.state.userData.profilePictureUrl;
 
     let className = 'Profile';
     if (isLoading) {
@@ -25,13 +44,15 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+        <img src={avatar} alt="" />
+</div>
         <div className="profile-body">
-          <h2>Name goes here</h2>
+          <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
-          <p>Bio goes here</p>
+          <p>{bio}</p>
           <h3>My friends</h3>
-          <Userlist usernames={[]} onChoose={this.props.onChoose} />
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
         </div>
       </div>
     );
